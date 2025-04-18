bbe6876e1d26ca31f4849bd534ab92b0f5a7a2f3
Derrick <lad@sheridancollege.ca>
2025-04-18 18:13:15 -0400

    my first commit

---
+++ src/main/java/com/laderrco/streamsage/StreamsageApplication.java
@@ -0,0 +1,13 @@
+package com.laderrco.streamsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laderrco/streamsage/StreamsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laderrco.streamsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
